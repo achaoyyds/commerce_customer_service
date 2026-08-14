70f7ed18bbe82698a8c0d5f0326f04d752535ea3
achao <2252439148@qq.com>
2026-08-14 16:31:11 +0800

    初始项目骨架包

---
+++ atuguigu/infrastructure/database.py
@@ -0,0 +1,11 @@
+"""
+sqlalchemy:名字sql log 幂
+作用：通过声明式方式让Python语言操作数据库（MySQL PGSQL...）
+使用：1. （声明）定义数据模型 2. 利用session对象通过API方式来交互数据库（crud）
+
+操作自己的数据库（customer_service） dialogue_states表（整个对话状态：聚合根）
+
+"""
+
+
+
