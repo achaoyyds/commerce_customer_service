dbf9b21958d029b1555efdd9fd98568d0467dc01
achao <2252439148@qq.com>
2026-08-15 20:38:36 +0800

    项目骨架文件:llm,database,config,http

--- atguigu/infrastructure/llm.py
+++ atguigu/infrastructure/llm.py
@@ -1,7 +1,7 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.language_models import BaseChatModel
 
-from atuguigu.config.config import settings
+from atguigu.config.config import settings
 
 llm: BaseChatModel = init_chat_model(
     model=settings.llm_model,
@@ -9,7 +9,6 @@ llm: BaseChatModel = init_chat_model(
     api_key=settings.llm_api_key,
     base_url=settings.llm_base_url,
     temperature=0  # 尽最大努力保证稳定性
-
 )
 
 if __name__ == '__main__':
